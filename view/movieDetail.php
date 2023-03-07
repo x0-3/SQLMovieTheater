b0cdc8d3dc246eea443f82c72386037bd4365b20
x0-3 <steph6363@gmail.com>
2023-03-07 15:00:29 +0100

    movie detail style finished

--- view/movieDetail.php
+++ view/movieDetail.php
@@ -7,14 +7,14 @@
         $movie = $stmt->fetch();
     ?>
 
-    <img src="<?=$movie['poster']?>" alt="moviePoster">
-
+    <figure>
+        <img src="<?=$movie['poster']?>" alt="moviePoster">
+    </figure>
     <div class="descrition">
         <h1><?=$movie['title']?></h1>
         <p>release date : <?=$movie['releaseDate']?></p>
         <p>Running time : <?=$movie['RunningTime']?></p>
-        <p>Producer : <?=$movie['familyName']?></p>
-        <p><?=$movie['NAME']?></p>
+        <p>Producer : <?=$movie['familyName']?> <?=$movie['NAME']?></p>
         <p>Genre : <?=$movie['genreName']?></p>
 
     </div>
