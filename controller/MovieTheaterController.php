ebd6311392ba83825d1997f06ec09de1a3face93
x0-3 <steph6363@gmail.com>
2023-03-09 09:13:32 +0100

    p

--- controller/MovieTheaterController.php
+++ controller/MovieTheaterController.php
@@ -10,8 +10,7 @@ class MovieTheaterController{
     // list of all the movies in db
     public function listFilms(){
         $pdo = connect::Connection();
-        $stmt = $pdo->query("
-            SELECT idMovie,title, DATE_FORMAT(releaseDateFrance,'%d/%m/%Y') AS releaseDay, poster
+        $stmt = $pdo->query("SELECT idMovie,title, DATE_FORMAT(releaseDateFrance,'%d/%m/%Y') AS releaseDay, poster
             FROM movie
         ");
 
@@ -21,8 +20,7 @@ class MovieTheaterController{
     // list of all the actors in db
     public function listActors(){
         $pdo = Connect::Connection();
-        $stmt = $pdo->query("
-        SELECT idActor, familyName, NAME, gender, DATE_FORMAT(birthday, '%d/%m/%Y') AS birthday, DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(),birthday)),'%Y')+0 AS age
+        $stmt = $pdo->query("SELECT idActor, familyName, NAME, gender, DATE_FORMAT(birthday, '%d/%m/%Y') AS birthday, DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(),birthday)),'%Y')+0 AS age
         FROM actor a
         INNER JOIN person p
         ON a.idPerson=p.idPerson
@@ -50,8 +48,7 @@ class MovieTheaterController{
     // list of all the producers in db
     public function listProducers(){
         $pdo = Connect ::Connection();
-        $stmt = $pdo->query("
-        SELECT m.idProducer,familyName,name,title, DATE_FORMAT(releaseDateFrance,'%d/%m/%Y') AS releaseDate
+        $stmt = $pdo->query("SELECT m.idProducer,familyName,name,title, DATE_FORMAT(releaseDateFrance,'%d/%m/%Y') AS releaseDate
         FROM movie m
         INNER JOIN producer pr 
         ON m.idProducer=pr.idProducer
@@ -75,7 +72,18 @@ class MovieTheaterController{
     // detail of one movie by it's ID
     public function movieDetail($id){
         $pdo = Connect::Connection();
-        $stmt = $pdo->prepare("SELECT mg.idGenre,m.idProducer,poster,title, DATE_FORMAT(releaseDateFrance,'%d/%m/%Y') AS releaseDate, TIME_FORMAT(SEC_TO_TIME(runningTime),'%H:%i') AS RunningTime, familyName,NAME, genreName FROM movie m INNER JOIN producer pr ON m.idProducer = pr.idProducer INNER JOIN person p ON pr.idPerson=p.idPerson INNER JOIN moviegenre mg ON mg.idMovie = m.idMovie INNER JOIN genre g ON g.idGenre = mg.idGenre WHERE m.idMovie = :id");
+        $stmt = $pdo->prepare("SELECT mg.idGenre,m.idProducer,poster,title, DATE_FORMAT(releaseDateFrance,'%d/%m/%Y') AS releaseDate, TIME_FORMAT(SEC_TO_TIME(runningTime),'%H:%i') AS RunningTime, familyName,NAME, genreName 
+        FROM movie m 
+        INNER JOIN producer pr 
+        ON m.idProducer = pr.idProducer 
+        INNER JOIN person p
+        ON pr.idPerson=p.idPerson 
+        INNER JOIN moviegenre mg 
+        ON mg.idMovie = m.idMovie 
+        INNER JOIN genre g 
+        ON g.idGenre = mg.idGenre 
+        WHERE m.idMovie = :id
+        ");
         $stmt->execute(["id"=>$id]);
         
         $stmt2 =$pdo->prepare("SELECT a.idActor,title, familyName, NAME, gender, roleName
@@ -108,7 +116,14 @@ class MovieTheaterController{
     // detail of one producer by it's ID
     public function producerDetail($id){
         $pdo = Connect::Connection();
-        $stmt = $pdo->prepare("SELECT m.idProducer,photo,familyName,NAME,gender,DATE_FORMAT(birthday,'%d/%m/%Y') AS birthday,DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(),birthday)),'%Y')+0 AS age,title, DATE_FORMAT(releaseDateFrance,'%d/%m/%Y') AS releaseDate FROM movie m INNER JOIN producer pr  ON m.idProducer=pr.idProducer INNER JOIN person p ON p.idPerson=pr.idPerson WHERE pr.idProducer= :id");
+        $stmt = $pdo->prepare("SELECT m.idProducer,photo,familyName,NAME,gender,DATE_FORMAT(birthday,'%d/%m/%Y') AS birthday,DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(),birthday)),'%Y')+0 AS age,title, DATE_FORMAT(releaseDateFrance,'%d/%m/%Y') AS releaseDate 
+        FROM movie m 
+        INNER JOIN producer pr  
+        ON m.idProducer=pr.idProducer 
+        INNER JOIN person p 
+        ON p.idPerson=pr.idPerson 
+        WHERE pr.idProducer= :id
+        ");
         $stmt->execute(["id"=>$id]);
         require "view/producerDetail.php";
     }
@@ -116,10 +131,17 @@ class MovieTheaterController{
     // detail of one genre by it's ID
     public function genreDetail($id){
         $pdo = Connect::Connection();
-        $stmt = $pdo->prepare("SELECT mg.idGenre,genreName,title,DATE_FORMAT(releaseDateFrance,'%d/%m/%Y'),synopsis FROM moviegenre mg INNER JOIN genre g ON mg.idGenre=g.idGenre INNER JOIN movie m ON m.idMovie=mg.idMovie WHERE mg.idGenre= :id");
+        $stmt = $pdo->prepare("SELECT mg.idGenre,genreName,title,DATE_FORMAT(releaseDateFrance,'%d/%m/%Y'),synopsis 
+        FROM moviegenre mg 
+        INNER JOIN genre g 
+        ON mg.idGenre=g.idGenre 
+        INNER JOIN movie m 
+        ON m.idMovie=mg.idMovie 
+        WHERE mg.idGenre= :id
+        ");
         $stmt->execute(["id"=>$id]);
 
-        $stmt2= $pdo->prepare("SELECT g.idGenre,title,genreName ,poster,DATE_FORMAT(releaseDateFrance,'%d/%m/%Y') AS releaseDate, TIME_FORMAT(SEC_TO_TIME(runningTime),'%H:%i') AS RunningTime
+        $stmt2= $pdo->prepare("SELECT m.idMovie,g.idGenre,title,genreName ,poster,DATE_FORMAT(releaseDateFrance,'%d/%m/%Y') AS releaseDate, TIME_FORMAT(SEC_TO_TIME(runningTime),'%H:%i') AS RunningTime
         FROM moviegenre mg
         INNER JOIN movie m
         ON mg.idMovie=m.idMovie
@@ -134,7 +156,12 @@ class MovieTheaterController{
     // detail of one genre by it's ID
     public function actorDetail($id){
         $pdo = Connect::Connection();
-        $stmt = $pdo->prepare("SELECT idActor,familyName,NAME,gender,DATE_FORMAT(birthday,'%d/%m/%Y') AS birthday,DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(),birthday)),'%Y')+0 AS age,photo FROM actor a INNER JOIN person p ON a.idPerson=p.idPerson WHERE idActor= :id");
+        $stmt = $pdo->prepare("SELECT idActor,familyName,NAME,gender,DATE_FORMAT(birthday,'%d/%m/%Y') AS birthday,DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(),birthday)),'%Y')+0 AS age,photo 
+        FROM actor a 
+        INNER JOIN person p 
+        ON a.idPerson=p.idPerson 
+        WHERE idActor= :id
+        ");
         $stmt->execute(["id"=>$id]);
         require "view/actorDetail.php";
     }
--- view/genreDetail.php
+++ view/genreDetail.php
@@ -1,29 +1,37 @@
 <?php ob_start(); ?>
 
+
+
+<section id="genreDetailSection">
+    <!-- fetch all the movies for that genre -->
+    <?php 
+    foreach($stmt2->fetchAll() as $movieGenre){ ?>
+
+        <a href="index.php?action=movieDetail&id=<?=$movieGenre['idMovie']?>">
+
+            <figure>
+                <img src="<?=$movieGenre['poster']?>" alt="movie poster">
+                
+                <figcaption>
+                    <p><?=$movieGenre['title']?></p>
+                    <p><?=$movieGenre['releaseDate']?></p>
+                    <p><?=$movieGenre['RunningTime']?></p>
+                </figcaption>
+            </figure>
+            
+        </a>
+    
+    <?php }?>
+</section>
+
+<!-- fetch the name of genres -->
 <?php 
 $genre = $stmt->fetch();
 ?>
 
-<section class="genreDetailSection">
-
-    <h2><?=$genre['genreName']?></h2>
-    
-    <?php 
-    foreach($stmt2->fetchAll() as $movieGenre){ ?>
-        <figure>
-            <img src="<?=$movieGenre['poster']?>" alt="movie poster">
-        </figure>
-
-        <p><?=$movieGenre['title']?></p>
-        <p><?=$movieGenre['releaseDate']?></p>
-        <p><?=$movieGenre['RunningTime']?></p>
-    
-    <?php }?>
-</section>
-    
 <?php 
 $title = "genre detail";
-$secondTitle = "Genre Description";
+$secondTitle = $genre['genreName'];
 $content = ob_get_clean();
 require "view/template.php";
 
