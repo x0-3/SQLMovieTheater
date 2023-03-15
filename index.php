1f420770ec86358633379741600ee6f0433c6c76
x0-3 <steph6363@gmail.com>
2023-03-15 13:45:14 +0100

    condition that allows certain type of img formats

--- index.php
+++ index.php
@@ -66,6 +66,13 @@ if(isset($_GET['action'])){
                     $uploadOk = 0;
                 }
 
+                // Allow certain file formats
+                if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+                && $imageFileType != "gif" ) {
+                echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+                $uploadOk = 0;
+                }
+
                 // Check if $uploadOk is set to 0 by an error
                 if ($uploadOk == 0) {
                     echo "Sorry, your file was not uploaded.";
@@ -133,6 +140,14 @@ if(isset($_GET['action'])){
                 if ($_FILES["photo"]["size"] > 500000) {
                     echo "Sorry, your file is too large.";
                     $uploadOk = 0;
+                }
+
+                
+                // Allow certain file formats
+                if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+                && $imageFileType != "gif" ) {
+                echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+                $uploadOk = 0;
                 }
 
                 // Check if $uploadOk is set to 0 by an error
@@ -238,9 +253,16 @@ if(isset($_GET['action'])){
                 }
 
                 // Check file size
-                if ($_FILES["poster"]["size"] > 500000) {
+                if ($_FILES["poster"]["size"] > 200000) {
                     echo "Sorry, your file is too large.";
                     $uploadOk = 0;
+                }
+
+                // Allow certain file formats
+                if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+                && $imageFileType != "gif" ) {
+                echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+                $uploadOk = 0;
                 }
 
                 // Check if $uploadOk is set to 0 by an error
